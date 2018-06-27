5215b4b3f5a489565016bee50a06f96eaebe5d38
Beta Faccion <betafcc@gmail.com>
2018-06-27 00:25:59 -0300

    Small refactor

--- dataclass_util/dict/__init__.py
+++ dataclass_util/dict/__init__.py
@@ -10,18 +10,15 @@ _map = map
 def map(f : Callable,
         *dicts : Dict,
         ) -> Dict:
-    common_keys = reduce(_intersection, _map(set, dicts))
+    common_keys = reduce(
+        lambda acc, n: acc.intersection(n),
+        _map(set, dicts),
+    )
 
     return {
         key: f(*_map(itemgetter(key), dicts))
         for key in common_keys
     }
-
-
-def _intersection(a : Set,
-                  b : Set,
-                  ) -> Set:
-    return a.intersection(b)
 
 
 def merge_with(f   : Callable,
