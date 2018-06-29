6a4eb838e18a832de8060598ae66606d79e67b81
Beta Faccion <betafcc@gmail.com>
2018-06-29 17:13:51 -0300

    Fixes operator module creation

--- dataclass_util/operator.py
+++ dataclass_util/operator.py
@@ -3,7 +3,7 @@ from dataclass import replace, asdict
 from .meta.operator import make_module
 
 
-locals().update(make_module(replace=replace, asdict=asdict))
+locals().update(make_module(replace=replace, asdict=asdict).__dict__)
 
 
 del make_module
