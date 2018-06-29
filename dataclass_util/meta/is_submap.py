e99efc5eef0b9065f1c77b192eb268ec5e8761d3
Beta Faccion <betafcc@gmail.com>
2018-06-29 16:54:12 -0300

    Adds meta package

---
+++ dataclass_util/meta/is_submap.py
@@ -0,0 +1,33 @@
+from types import SimpleNamespace
+from operator import eq
+
+
+def make_module(asdict, getattr=getattr):
+    def is_key_submap(a, b):
+        return is_submap_by(lambda _a, _b: True, a, b)
+
+
+    def is_submap(a, b):
+        return is_submap_by(eq, a, b)
+
+
+    def is_submap_by(f, a, b):
+        for k, v in asdict(a).items():
+            try:
+                if not f(getattr(b, k), v):
+                    return False
+            except AttributeError:
+                return False
+
+        return True
+
+
+    def is_proper_submap(a, b):
+        return is_proper_submap_by(eq, a, b)
+
+
+    def is_proper_submap_by(f, a, b):
+        raise NotImplementedError
+
+
+    return SimpleNamespace(**locals())
