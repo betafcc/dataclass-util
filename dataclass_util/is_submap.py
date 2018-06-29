342ef5376254370cff81f7dbc3617dbf3b6ab6f4
Beta Faccion <betafcc@gmail.com>
2018-06-29 14:35:18 -0300

    Implements is_submap_of_by

--- dataclass_util/is_submap.py
+++ dataclass_util/is_submap.py
@@ -1,4 +1,5 @@
 from operator import eq
+from dataclasses import asdict
 
 
 def is_submap_of(a, b):
@@ -6,8 +7,14 @@ def is_submap_of(a, b):
 
 
 def is_submap_of_by(f, a, b):
-    raise NotImplementedError
+    for k, v in asdict(a).items():
+        try:
+            if not f(getattr(b, k), v):
+                return False
+        except AttributeError:
+            return False
 
+    return True
 
 def is_proper_submap_of(a, b):
     return is_proper_submap_of_by(eq, a, b)
