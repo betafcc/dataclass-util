baab8fbc522d5eb4af99ca20eceeac0329581588
Beta Faccion <betafcc@gmail.com>
2018-06-29 16:56:14 -0300

    Refactors is_submap to use meta

--- dataclass_util/is_submap.py
+++ dataclass_util/is_submap.py
@@ -1,29 +1,9 @@
-from operator import eq
 from dataclasses import asdict
 
-
-def is_key_submap(a, b):
-    return is_submap_by(lambda _a, _b: True, a, b)
+from .meta.is_submap import make_module
 
 
-def is_submap(a, b):
-    return is_submap_by(eq, a, b)
+locals().update(make_module(asdict).__dict__)
 
 
-def is_submap_by(f, a, b):
-    for k, v in asdict(a).items():
-        try:
-            if not f(getattr(b, k), v):
-                return False
-        except AttributeError:
-            return False
-
-    return True
-
-
-def is_proper_submap(a, b):
-    return is_proper_submap_by(eq, a, b)
-
-
-def is_proper_submap_by(f, a, b):
-    raise NotImplementedError
+del make_module
