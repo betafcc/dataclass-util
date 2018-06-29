2550d21fe86fd8528dda55117aa8deb2ac84a94a
Beta Faccion <betafcc@gmail.com>
2018-06-29 00:34:02 -0300

    Starts complete rewrite

--- dataclass_util/dataclass/broadcast_operator.py
+++
@@ -1,9 +0,0 @@
-from dataclass_util.meta import broadcast_operators
-from . import map as _map
-
-
-locals().update(broadcast_operators(_map).__dict__)
-
-
-del broadcast_operators
-del _map
--- dataclass_util/dataclass/elementwise_operator.py
+++
@@ -1,9 +0,0 @@
-from dataclass_util.meta import elementwise_operators
-from . import merge_with
-
-
-locals().update(elementwise_operators(merge_with).__dict__)
-
-
-del elementwise_operators
-del merge_with
