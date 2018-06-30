c2556e9039a545e42a42faefbc49898073eeef27
Beta Faccion <betafcc@gmail.com>
2018-06-30 19:41:02 -0300

    Implements basic class_.elementwise

--- dataclass_util/class_.py
+++ dataclass_util/class_.py
@@ -1,2 +1,29 @@
+import operator
+
+from dataclass_util.operator import elementwise as _elementwise
+
+
 def elementwise(cls):
-    raise NotImplementedError
+    cls = type(cls.__name__, cls.__bases__, dict(cls.__dict__))
+
+    names = {
+        '__add__',
+        '__sub__',
+        '__mul__',
+        '__matmul__',
+        '__truediv__',
+        '__floordiv__',
+        '__mod__',
+        # '__divmod__',
+        '__pow__',
+        '__lshift__',
+        '__rshift__',
+        '__and__',
+        '__xor__',
+        '__or__',
+    }
+
+    for name in names:
+        setattr(cls, name, _elementwise(getattr(operator, name)))
+
+    return cls
